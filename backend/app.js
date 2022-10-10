c333682f018c6a9a7cce090f627e6709e8c3eb0a
Corey Lott <Lott-c@hotmail.com>
2022-10-10 13:00:22 -0500

    routes, controllers, models setup/moving on to test routes

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 import express, { application } from 'express';
-
+import mongoose from 'mongoose';
+import router from "./routes/routes"
 const app = express();
 
-app.use("/", (req, res, next) => {
-    res.send("Hello World!");
-})
-
-app.listen(3000);
+app.use("/api/user", router);
+mongoose.connect('mongodb+srv://corey:corey@blogwm.iiamxai.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>app.listen(3000))
+.then(()=>console.log("Connected to Database and Listening to Local Host 3000"))
+.catch((err)=>console.log(err));

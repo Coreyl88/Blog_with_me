003f153d5769c6c43cac1ebc8edd6f98573debbc
Corey Lott <Lott-c@hotmail.com>
2022-10-14 10:39:18 -0500

    updating files

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-import express, { application } from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
 import blogRouter from './routes/blog_routes';
 import router from "./routes/routes"
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
-mongoose.connect('mongodb+srv://corey:corey@blogwm.iiamxai.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('MONGDB_URI')
 .then(()=>app.listen(4000))
 .then(()=>console.log("Connected to Database and Listening to Local Host 4000"))
 .catch((err)=>console.log(err));
